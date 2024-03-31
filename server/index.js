90a7632dbd641adaed6051a21ad82946625eb12e
Srushti2411 <srushtigarad24@gmail.com>
2024-03-31 16:48:19 +0530

    Fullstack application of Notes App

---
+++ server/index.js
@@ -0,0 +1,111 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose, {model,Schema} from 'mongoose';
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+const connectDB = async () =>{
+    await mongoose.connect("mongodb+srv://srushtigarad24:srushtigarad24@cluster1.0n6wbyy.mongodb.net/notes")
+    console.log("Database Connected")
+}
+connectDB();
+
+const PORT = 5000;
+
+const noteSchema = new Schema({
+    title : String,
+    content : String ,
+    category : String 
+})
+
+const Note = model("Note", noteSchema);
+
+app.get('/health',(req,res)=>{
+    res.json({
+        success : true,
+        message : 'Server is Healthy',
+        data : null
+    })
+})
+
+
+app.post('/notes',async(req,res)=>{
+    const {title,content,category} = req.body
+
+    const newNote = await Note.create({
+        "title": title,
+        "content": content ,
+        "category" : category
+    })
+
+    res.json({
+        success: true,
+        message : "Notes added successfully",
+        data : newNote
+
+    })
+})
+
+app.get('/notes',async(req,res)=>{
+
+    const notes = await Note.find();
+
+    res.json({
+        success : true,
+        message : 'Notes fetched successfully',
+        data: notes
+    })
+})
+
+app.get('/notes/:id',async(req,res)=>{
+    const {id} = req.params;
+
+    const note = await Note.findOne({
+        _id : id
+    })
+
+    res.json({
+        success :true,
+        message : "Notes fetched successfully",
+        data : note
+
+    })
+})
+
+app.put('/notes/:id',async(req,res)=>{
+    const {id} = req.params;
+
+    const {title,content,category} = req.body;
+
+    await Note.updateOne({_id: id},{$set: {
+        title : title,
+        content : content,
+        category : category
+    }})
+    res.json({
+        success:true,
+        message : 'Notes updated successfully',
+        data : null
+    })
+})
+
+app.delete('/notes/:id',async(req,res)=>{
+    const {id} = req.params;
+
+    await Note.deleteOne({_id: id})
+
+    res.json({
+        success : true,
+        message:"Note deleted successfully",
+        data : null
+    })
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`server is running on ${PORT} port` )
+});
+
+

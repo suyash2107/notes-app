0519a5702590cb98ab3de383f771aa587616ece8
Srushti2411 <srushtigarad24@gmail.com>
2024-03-31 21:44:39 +0530

    Home rendering created

---
+++ client/src/views/Home/Home.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import './Home.css'
+
+function Home() {
+    const [notes, setNotes] = useState([]);
+
+    const loadNotes = async()=>{
+        const response = await axios.get('http://localhost:5000/notes');
+        console.log(response.data.data);
+        setNotes(response.data.data);
+    }
+    useEffect(()=>{
+        loadNotes();
+    },[])
+
+  return (
+    <div>
+      <h1>Home</h1>
+
+        {
+            notes.map((note , index)=>{
+
+                return(
+                    <div>
+                        <h3>{note.title}</h3>
+                        <p>{note.content}</p>
+                        <span>{note.category}</span>
+                    </div>
+                )
+            })
+        }
+
+
+    </div>
+  )
+}
+
+export default Home
---
+++ client/src/views/NewNote/NewNote.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NewNote.css'
+
+function NewNote() {
+  return (
+    <div>
+      <h1>NewNode</h1>
+    </div>
+  )
+}
+
+export default NewNote
